6c4ba899fc4398a1e2a463511a34e73fe01a0ab3
stefan504 <63974903+stefan504@users.noreply.github.com>
2020-12-12 21:23:44 +0100

    Add files via upload

---
+++ Modal/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// const modal = document.querySelector('.modal');
+
+// const overlay = document.querySelector('.overlay');
+
+// const btnCloseModal = document.querySelector('.close-modal');
+
+// const btnsOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnsOpenModal);
+
+// const openModal = function () {
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
+
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
+
+// document.addEventListener('keydown', function (e) {
+//   console.log(e.key);
+
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(3, 5);
+console.log(appleJuice);
